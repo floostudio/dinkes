16223d350ce933aea8769d107238d2820e283c6b
Iqbal Permana <opreks@gmail.com>
2016-03-21 13:24:11 +0700

    Init Project

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '4dm1n@3juk5u5';
+$db['default']['password'] = '';
 $db['default']['database'] = 'ejuksus2016';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
